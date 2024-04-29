e48c0afcc0425e689a848d47cf7a0af770c1d5dc
drex007 <vykeoj@gmail.com>
2024-04-29 18:02:44 +0100

    feat: csrf fixess and corrections

--- task/admin.py
+++ task/admin.py
@@ -14,12 +14,27 @@ class TaskModelReward(admin.ModelAdmin):
     list_display = [field.name for field in TaskModel._meta.fields if field.name != "id"]
     
     def disburse_rewards(self,request, queryset):
+        reward = TaskRewardModel.objects.filter().first()
         for x in queryset:
             if x.reward_disbursed == False:
-                x.account.all().update(points = F('points') + 1000)
+                if x.task_category == "Like":
+                    x.account.all().update(points = F('points') + int(reward.like))
+                if x.task_category == "Follow":
+                    x.account.all().update(points = F('points') + int(reward.follow))
+                if x.task_category == "Join":
+                    x.account.all().update(points = F('points') + int(reward.join))
+                if x.task_category == "Retweet":
+                    x.account.all().update(points = F('points') + int(reward.retweet))
+                if x.task_category == "Quote":
+                    x.account.all().update(points = F('points') + int(reward.quote))
+                if x.task_category == "Comment":
+                    x.account.all().update(points = F('points') + int(reward.comment))
+                if x.task_category == "Others":
+                    x.account.all().update(points = F('points') + int(reward.follow))
+                else:
+                    x.account.all().update(points = F('points') + int(reward.like))
                 x.reward_disbursed = True
                 x.save()
-            
 
 admin.site.register(TaskRewardModel)
 admin.site.register(TaskModel, TaskModelReward)
